ea78978f5b66ed2b61bd6384d497b86ee739123e
Jonas Muñiz <jonasmz.dev@gmail.com>
2024-02-18 02:20:09 -0300

    Implementacion de presentadores

---
+++ 3.InterfaceAdapters/Presenters/NorthWind.Sales.Backend.Presenters/CreateOrder/CreateOrderPresenter.cs
@@ -0,0 +1,13 @@
+namespace NorthWind.Sales.Backend.Presenters.CreateOrder
+{
+    internal class CreateOrderPresenter : ICreateOrderOutputPort
+    {
+        public int OrderId {get; private set;}
+
+        public Task Handle(OrderAggregate addedOrder)
+        {
+            OrderId = addedOrder.Id;
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ 3.InterfaceAdapters/Presenters/NorthWind.Sales.Backend.Presenters/GlobalUsings.cs
@@ -0,0 +1,4 @@
+global using NorthWind.Sales.Backend.BusinessObjects.Aggregates;
+global using NorthWind.Sales.Backend.BusinessObjects.Interfaces.CreateOrder;
+global using Microsoft.Extensions.DependencyInjection;
+global using NorthWind.Sales.Backend.Presenters.CreateOrder;
