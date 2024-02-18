a436247c56f3bf1e169e5045d39b4fca4bacf464
Jonas Muñiz <jonasmz.dev@gmail.com>
2024-02-18 00:40:36 -0300

    Termino de definir objetos de negocio para northwind sales

--- 2.Application.business.Rules/NorthWind.Sales.Backend.BusinessObjects/Aggregates/OrderAggregate.cs
+++ 2.Application.business.Rules/NorthWind.Sales.Backend.BusinessObjects/Aggregates/OrderAggregate.cs
@@ -6,7 +6,29 @@ namespace NorthWind.Sales.Backend.BusinessObjects.Aggregates
         public IReadOnlyCollection<OrderDetail> OrderDetails => OrderDetailField;
 
         public void AddDetail( int productId, decimal unitPrice, short quantity){
+            var ExistingOrderDetail = OrderDetailField.FirstOrDefault(od => od.ProductId == productId);
+            if(ExistingOrderDetail != default){
+                quantity += ExistingOrderDetail.Quantity;
+                OrderDetailField.Remove(ExistingOrderDetail);
+            }
 
+            OrderDetailField.Add( new OrderDetail(productId, unitPrice, quantity) );
+        }
+
+        public static OrderAggregate From(CreateOrderDTO orderDTO){
+            var OrderAggregate = new OrderAggregate{
+                CustomerId = orderDTO.CustomerId,
+                ShipAddress = orderDTO.ShipAddress,
+                ShipCity = orderDTO.ShipCity,
+                ShipCountry = orderDTO.ShipCountry,
+                ShipPostalCode = orderDTO.ShipPostalCode
+            };
+
+            foreach(var item in orderDTO.OrderDetails){
+                OrderAggregate.AddDetail(item.ProductId, item.UnitPrice, item.Quantity);
+            }
+
+            return OrderAggregate;
         }
     }
 }
